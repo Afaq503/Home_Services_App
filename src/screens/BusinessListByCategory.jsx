849e20e260c61a74440f6f9794321995d7962898
Afaq503 <afaqg503@gmail.com>
2024-03-08 20:07:54 +0500

    done business

--- src/screens/BusinessListByCategory.jsx
+++ src/screens/BusinessListByCategory.jsx
@@ -4,23 +4,22 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import GlobalApi from "../utils/GlobalApi";
 import BusinessListItem from "./BusinessListItem";
+import colors from "../utils/colors";
 
 const BusinessListByCategory = () => {
   const param = useRoute().params;
   const navigation = useNavigation();
-  const [businessList, setBusinessList] = useState([])
+  const [businessList, setBusinessList] = useState([]);
 
   useEffect(() => {
-    console.log("Category:", param.category);
-    param &&  getBusinessByCategory()
+    param && getBusinessByCategory();
   }, [param]);
 
   const getBusinessByCategory = () => {
-    GlobalApi.getBusinessListByCategory(param.category).then(res => {
-        console.log(`this is responc for category: ${res.businessLists}`);
-        setBusinessList(res.businessLists)
-    })
-  }
+    GlobalApi.getBusinessListByCategory(param.category).then((res) => {
+      setBusinessList(res.businessLists);
+    });
+  };
 
   return (
     <View style={{ padding: 20, paddingTop: 30 }}>
@@ -32,20 +31,32 @@ const BusinessListByCategory = () => {
           gap: 10,
         }}
       >
-        <TouchableOpacity onPress={()=> navigation.goBack()}>
-        <Ionicons name="arrow-back-outline" size={24} color={"black"} />
-
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back-outline" size={24} color={"black"} />
         </TouchableOpacity>
         <Text style={{ fontSize: 24, fontFamily: "outfit-medium" }}>
           {param?.category}
         </Text>
       </View>
-      <FlatList
-      data={businessList}
-      renderItem={({item,index}) => (
-        <BusinessListItem business={item}/>
+      {businessList?.length > 0 ? (
+        <FlatList
+          data={businessList}
+          style={{ marginTop: 15 }}
+          renderItem={({ item, index }) => <BusinessListItem business={item} />}
+        />
+      ) : (
+        <Text
+          style={{
+            fontFamily: "outfit-medium",
+            fontSize: 20,
+            textAlign: "center",
+            marginTop: "20%",
+            color: colors.GRAY
+          }}
+        >
+          No Business Found
+        </Text>
       )}
-      />
     </View>
   );
 };
--- src/screens/BusinessListItem.jsx
+++ src/screens/BusinessListItem.jsx
@@ -1,31 +1,55 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import colors from "../utils/colors";
+import { Ionicons } from "@expo/vector-icons";
 
-import colors from '../utils/colors'
-
-const BusinessListItem = ({business}) => {
+const BusinessListItem = ({ business }) => {
   return (
     <View style={styles.container}>
-        <Image
-        source={{uri:business?.images[0]?.url}}
-        style={styles.image}
-        />
+      <Image source={{ uri: business?.images[0]?.url }} style={styles.image} />
+      <View style={styles.subContaner}>
+        <Text
+          style={{ fontFamily: "outfit", color: colors.GRAY, fontSize: 15 }}
+        >
+          {business.contactPerson}
+        </Text>
+        <Text style={{ fontFamily: "outfit-bold", fontSize: 19 }}>
+          {business.name}
+        </Text>
+        <Text
+          style={{ fontFamily: "outfit-medium", color: colors.GRAY, fontSize: 16 }}
+        >
+          <Ionicons
+            name="location"
+            size={20}
+            color={colors.PRIMARY}
+          />
+                 {business.address}
+        </Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        padding:10,
-        backgroundColor: colors.WHITE,
-        borderRadius:15,
-        marginBottom:15
-    },
-    image:{
-        width:100,
-        height:100,
-        borderRadius:15
-    }
-})
+  container: {
+    padding: 10,
+    backgroundColor: colors.WHITE,
+    borderRadius: 15,
+    marginBottom: 15,
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+  },
+  subContaner: {
+    display: "flex",
+    gap: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 15,
+  },
+});
 
-export default BusinessListItem
+export default BusinessListItem;
