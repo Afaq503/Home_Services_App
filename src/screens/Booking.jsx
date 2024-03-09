dd851e8186c549fa475886e44def501f6e283a84
Afaq503 <afaqg503@gmail.com>
2024-03-09 20:05:52 +0500

    Final touch

--- src/screens/Booking.jsx
+++ src/screens/Booking.jsx
@@ -1,25 +1,24 @@
 import { View, Text, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
-import PageHeading from "../components/PageHeading";
 import GlobalApi from "../utils/GlobalApi";
 import { useUser } from "@clerk/clerk-expo";
 import BusinessListItem from "./BusinessListItem";
 
 const Booking = () => {
   const { user } = useUser();
-  const [bookingList, setBookingList] = useState();
-  const [loading, setLoading] =useState(false)
+  const [bookingList, setBookingList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     user && getUserBooking();
   }, [user]);
   const getUserBooking = () => {
-    setLoading(true)
+    setLoading(true);
     GlobalApi.getUserBooking(user.primaryEmailAddress.emailAddress).then(
       (res) => {
         console.log(res);
         setBookingList(res.bookings);
-        setLoading(false)
+        setLoading(false);
       }
     );
   };
@@ -31,19 +30,16 @@ const Booking = () => {
       </Text>
 
       <View>
+      
         <FlatList
           data={bookingList}
-          onRefresh={() =>getUserBooking() }
+          onRefresh={() => getUserBooking()}
           refreshing={loading}
           renderItem={({ item, index }) => (
             <View>
-              <BusinessListItem
-                business={item?.businessLists}
-                booking={item}
-              />
+              <BusinessListItem business={item?.businessLists} booking={item} />
             </View>
           )}
-          keyExtractor={(item, index) => index.toString()}
         />
       </View>
     </View>
